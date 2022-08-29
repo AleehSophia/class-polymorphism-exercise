1828b27f39502c2fe28cd6fdc42fb6f89f8e1617
AleehSophia <alessia.casado@gmail.com>
2022-08-29 20:07:25 -0300

    feat: main implementation

--- app/Program.java
+++ app/Program.java
@@ -1,7 +1,61 @@
 package app;
 
-public class Program {
-    public static void main(String[] args) {
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            System.out.println("Product #" + i + " data:");
+            System.out.print("Common, used or imported (c/u/i)?");
+            char ch = sc.next().charAt(0);
+
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+
+            if(ch == 'i') {
+                System.out.print("Customs fee: ");
+                double fee = sc.nextDouble();
+                Product product = new ImportedProduct(name, price, fee);
+                list.add(product);
+            } else if (ch == 'u') {
+                System.out.print("Manufactured date (DD/MM/YYYY):");
+                Date date = sfd.parse(sc.next());
+                Product product = new UsedProduct(name, price, date);
+                list.add(product);
+            }
+            else {
+                Product product = new Product(name, price);
+                list.add(product);
+            }
+        }
+
+        System.out.println();
+        System.out.println("PRICE TAGS:");
+        for (Product product : list) {
+            System.out.println(product.priceTag());
+        }
+
+        sc.close();
     }
 }
