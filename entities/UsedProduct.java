87b29305c74abcf6e6ab22e13564de7aacd5c5a1
AleehSophia <alessia.casado@gmail.com>
2022-08-29 20:05:03 -0300

    Fix: priceTag implementation fixed

--- entities/UsedProduct.java
+++ entities/UsedProduct.java
@@ -1,8 +1,11 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UsedProduct extends Product {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     private Date manufactureDate;
 
@@ -25,6 +28,11 @@ public class UsedProduct extends Product {
 
     @Override
     public String priceTag() {
-        return super.priceTag();
+        return getName()
+                + " (used) $ "
+                + String.format("%.2f", getPrice())
+                + " (Manufacture date: "
+                + sdf.format(manufactureDate)
+                + ")";
     }
 }
